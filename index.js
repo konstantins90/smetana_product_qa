1aed776aa0b2285672bd84577df6206efe74300e
Konstantin Smetana <konstantinsmetana@gmail.com>
2024-08-30 22:53:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+
+const token = '7531164983:AAFaCWeC3iTG_7VQXiPQSRT4BsSyX2IUgqM'
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
