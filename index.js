9dbd8401ff4ae9bfc4950f473af33f2a1f64376f
Konstantin Smetana <konstantinsmetana@gmail.com>
2024-09-02 21:07:37 +0200

    require statt import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { Telegraf } from 'telegraf'
-import { message } from 'telegraf/filters'
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
