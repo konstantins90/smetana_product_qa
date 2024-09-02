7ea398c1251197ae1eb917113eebf753323e70f2
Konstantin Smetana <konstantinsmetana@gmail.com>
2024-09-02 21:10:11 +0200

    add env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { Telegraf } = require('telegraf');
-const { message } = require('telegraf/filters');
+require('dotenv').config()
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
